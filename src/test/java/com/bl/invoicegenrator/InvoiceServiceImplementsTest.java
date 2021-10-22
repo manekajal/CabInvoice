54adb5a00c50c4c59c552f3c75cffa36a2d73f1a
manekajal <manekajal2014@gmail.com>
2021-10-22 20:15:09 +0530

    [Kajal]: Calculate Total Fare:

--- src/test/java/com/bl/invoicegenrator/InvoiceServiceImplementsTest.java
+++ src/test/java/com/bl/invoicegenrator/InvoiceServiceImplementsTest.java
@@ -1,5 +1,6 @@
 package com.bl.invoicegenrator;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,9 +16,17 @@ public class InvoiceServiceImplementsTest {
            double distance = 2.0;
            int time = 5;
         double fare=  invoiceGenerator.calculateFare(distance,time );
-        assertEquals(25,fare,0.0);
+        Assert.assertEquals(25,fare,0.0);
 
        }
-
+       @Test
+        public void givenLessDistanceOrTime_ShouldReturnMinFare()
+        {
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            double distance = 0.1;
+            int time = 1;
+            double fare = invoiceGenerator.calculateFare( distance, time);
+            assertEquals(5, fare, 0.0);
+        }
 
 }
